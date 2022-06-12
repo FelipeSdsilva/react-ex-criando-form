aa065496c34760a8d4e9658891bec8197c15e8f8
Felipe Sousa <felipe.fps09@hotmail.com>
2022-06-12 09:56:24 -0300

    Resoliction problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
+import React, { useState } from "react";
+
 const App = () => {
+  
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState('');
+
+  const nameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
+  const lastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  }
+
+  const ageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(event.target.value);
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={nameChange} />
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={lastName} onChange={lastNameChange} />
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={age} onChange={ageChange} />
       </div>
 
-      <hr/>
+      <hr />
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {`${name} ${lastName}`}, tudo bem?<br />
+      Você tem {age} anos.
     </div>
   );
 }
